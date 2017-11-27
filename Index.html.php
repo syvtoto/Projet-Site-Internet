98d9368c8555c8e904e0d0b774ce82d5070977c7
Thibaut-Comte <thibaut.comte.1@gmail.com>
2017-11-27 10:35:06 +0100

    route css

--- Index.html.php
+++ Index.html.php
@@ -17,7 +17,7 @@ informations : voir https://www.facebook.com/events/189882631576619/?active_tab=
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
           integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="./Index.css" media="screen,projection" type="text/css">
+    <link rel="stylesheet" href="./style/Index.css" media="screen,projection" type="text/css">
     <title>Site Internet Evenementiel</title>
 </head>
 <body class="container-fluid">
