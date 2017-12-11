2f0798808f29df6afa3a12575efe86d4c8dab115
PC-THOMAS\Thomas <thomas.flamant96@gmail.com>
2017-12-11 16:51:06 +0100

    mis en place de 2 fonds

--- Index.php
+++ Index.php
@@ -20,7 +20,7 @@ informations : voir https://www.facebook.com/events/189882631576619/?active_tab=
         <div class="col-xs-offset-1 col-xs-4">
             <img src="./images/bandeau.jpg" class="img-responsive center-block">
         </div>
-        <div class="col-xs-4 col-xs-offset-2">
+        <div class="col-xs-offset-2 col-xs-4">
             <div class="row">
                 <div class="col-xs-7">
                     <h5>Bienvenue</h5>
@@ -34,7 +34,7 @@ informations : voir https://www.facebook.com/events/189882631576619/?active_tab=
     </div>
 </header>
 <main>
-    <div class="container">
+    <div class="container MainC">
         <div class="row">
             <div class="col-xs-3 top">
                 <img src="./images/iconeGG.jpg" class="img-responsive center-block img">
