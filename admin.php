33290a815065606fff2872a47f7c318fac143f4f
Thibaut-Comte <thibaut.comte.1@gmail.com>
2018-01-04 17:19:53 +0100

    listing par validation /admin

--- admin.php
+++ admin.php
@@ -9,7 +9,7 @@
 <h1>Gestion des inscriptions</h1>
 <?php
 session_start();
-if (!isset($_SESSION['admin'])){
+if (!isset($_SESSION['admin'])) {
     header('location: login.php');
 }
 require 'includes/pdo.php';
@@ -21,27 +21,41 @@ $sql = 'SELECT * FROM inscriptions ORDER BY valide';
 $prep = $pdo->prepare($sql);
 $prep->execute();
 $inscrits = $prep->fetchAll();
-
+?>
+<h2>Participants à valider</h2>
+<?php
 foreach ($inscrits as $inscrit) {
-    if ($inscrit['valide']==1){
+    if ($inscrit['valide'] == 1) {
         $id = $inscrit['id'];
-        echo "</br>la liste des participants à validés : </br>"."<p>".$inscrit['civilite']." ".$inscrit['nom']." ".$inscrit['prenom']." "
-            .$inscrit['adresse']." ".$inscrit['cp']." ".$inscrit['ville']." ".$inscrit['dateNaissance']." ".$inscrit['email']." "
-            .$inscrit['membre']." ".$inscrit['jeux'];
+        echo "<p>" . $inscrit['civilite'] . " " . $inscrit['nom'] . " " . $inscrit['prenom'] . " "
+            . $inscrit['adresse'] . " " . $inscrit['cp'] . " " . $inscrit['ville'] . " " . $inscrit['dateNaissance'] . " " . $inscrit['email'] . " "
+            . $inscrit['membre'] . " " . $inscrit['jeux'];
         ?>
         <form method="get" action="check/valider.php">
-            <button type="submit" name="valider" value = "<?php echo $id; ?>">Valider</button>
-            <button type="submit" name="refuser" value = "<?php echo $id; ?>">Refuser</button>
+            <button type="submit" name="valider" value="<?php echo $id; ?>">Valider</button>
+            <button type="submit" name="refuser" value="<?php echo $id; ?>">Refuser</button>
         </form>
         <?php
-    } else if ($inscrit['valide']==2){
-        echo "</br>la liste des participants acceptés : </br>"."<p>".$inscrit['civilite']." ".$inscrit['nom']." ".$inscrit['prenom']." "
-            .$inscrit['adresse']." ".$inscrit['cp']." ".$inscrit['ville']." ".$inscrit['dateNaissance']." ".$inscrit['email']." "
-            .$inscrit['membre']." ".$inscrit['jeux']."</p>";
-} else if ($inscrit['valide']==0){
-        echo "</br>la liste des participants refusés : </br>"."<p>".$inscrit['civilite']." ".$inscrit['nom']." ".$inscrit['prenom']." "
-            .$inscrit['adresse']." ".$inscrit['cp']." ".$inscrit['ville']." ".$inscrit['dateNaissance']." ".$inscrit['email']." "
-            .$inscrit['membre']." ".$inscrit['jeux']."</p>";
+    }
+}
+?>
+<h2>Liste des participants</h2>
+<?php
+foreach ($inscrits as $inscrit) {
+    if ($inscrit['valide'] == 2) {
+        echo "<p>" . $inscrit['civilite'] . " " . $inscrit['nom'] . " " . $inscrit['prenom'] . " "
+            . $inscrit['adresse'] . " " . $inscrit['cp'] . " " . $inscrit['ville'] . " " . $inscrit['dateNaissance'] . " " . $inscrit['email'] . " "
+            . $inscrit['membre'] . " " . $inscrit['jeux'] . "</p>";
+    }
+}
+?>
+<h2>Participants refusés</h2>
+<?php
+foreach ($inscrits as $inscrit) {
+    if ($inscrit['valide'] == 0) {
+        echo "</br>la liste des participants refusés : </br>" . "<p>" . $inscrit['civilite'] . " " . $inscrit['nom'] . " " . $inscrit['prenom'] . " "
+            . $inscrit['adresse'] . " " . $inscrit['cp'] . " " . $inscrit['ville'] . " " . $inscrit['dateNaissance'] . " " . $inscrit['email'] . " "
+            . $inscrit['membre'] . " " . $inscrit['jeux'] . "</p>";
 
     }
 }
