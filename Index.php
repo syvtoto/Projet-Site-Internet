dfa6905ad70a3b0e9505fd25b8beffb8154ef21d
PC-THOMAS\Thomas <thomas.flamant96@gmail.com>
2018-01-04 16:23:46 +0100

    fonds ok

--- Index.php
+++ Index.php
@@ -12,7 +12,8 @@ informations : voir https://www.facebook.com/events/189882631576619/?active_tab=
 <head>
     <?php require 'includes/head.html'; ?>
     <link rel="stylesheet" href="./style/Index.css" media="screen,projection" type="text/css">
-    <link rel="stylesheet" href="./style/border.css" media="screen,projection" type="text/css"> // si bordure
+<!--    <link rel="stylesheet" href="./style/border.css" media="screen,projection" type="text/css">-->
+<!--    si bordure-->
     <title>Deuxième Soirée Jeux</title>
 </head>
 <body class="container-fluid">
@@ -20,7 +21,7 @@ informations : voir https://www.facebook.com/events/189882631576619/?active_tab=
     <?php require 'includes/header.html'; ?>
 </header>
 <main>
-    <div class="container MainC">
+    <div class="container">
         <div class="row">
             <div class="col-xs-3 top">
                 <img src="./images/iconeGG.jpg" class="img-responsive center-block img">
@@ -33,7 +34,7 @@ informations : voir https://www.facebook.com/events/189882631576619/?active_tab=
                     <p>75015 Paris</p>
                 </div>
             </div>
-            <div class="col-xs-9">
+            <div class="col-xs-9 MainC">
                 <div class="row">
                     <div class="col-xs-12 center top">
                         <p>De nombreux jeux seront mis a disposition tels que :</p>
@@ -43,27 +44,27 @@ informations : voir https://www.facebook.com/events/189882631576619/?active_tab=
                 <div class="row">
                     <div class="col-xs-12">
                         <div class="row">
-                            <div class="col-xs-6">
+                            <div class="col-xs-offset-2 col-xs-4">
                                 <img src="images/Plateau/summonerWar.jpg" class="img-responsive center-block img">
                             </div>
-                            <div class="col-xs-6">
+                            <div class="col-xs-4">
                                 <img src="images/Plateau/dominion.jpg" class="img-responsive center-block img">
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col-xs-6">
+                            <div class="col-xs-offset-2 col-xs-4">
                                 <img src="images/Plateau/mysterirum.jpg" class="img-responsive center-block img">
                             </div>
-                            <div class="col-xs-6">
+                            <div class="col-xs-4">
                                 <img src="images/Plateau/les-contrees-de-l-horreur.jpg"
                                      class="img-responsive center-block img">
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col-xs-6">
+                            <div class="col-xs-offset-2 col-xs-4">
                                 <img src="images/Plateau/horreur-a-arkham.jpg" class="img-responsive center-block img">
                             </div>
-                            <div class="col-xs-6">
+                            <div class="col-xs-4">
                                 <img src="images/Plateau/Captain_Sonar.jpg" class="img-responsive center-block img">
                             </div>
                         </div>
@@ -77,22 +78,22 @@ informations : voir https://www.facebook.com/events/189882631576619/?active_tab=
                 <div class="row">
                     <div class="col-xs-12">
                         <div class="row">
-                            <div class="col-xs-6">
+                            <div class="col-xs-offset-2 col-xs-4">
                                 <img src="images/Cartes/smashUp.jpg" class="img-responsive center-block img">
                             </div>
-                            <div class="col-xs-6">
+                            <div class="col-xs-4">
                                 <img src="images/Cartes/JungleSpeed.jpg" class="img-responsive center-block img">
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col-xs-6">
+                            <div class="col-xs-offset-2 col-xs-4">
                                 <img src="images/Cartes/munchkin.jpg" class="img-responsive center-block img">
                             </div>
-                            <div class="col-xs-6">
+                            <div class="col-xs-4">
                                 <img src="images/Cartes/magic-the-gathering.jpg"
                                      class="img-responsive center-block img">
                             </div>
-                            <div class="col-xs-6">
+                            <div class="col-xs-4">
                                 <img src="images/Cartes/TAROT.jpg" class="img-responsive center-block img">
                             </div>
                         </div>
@@ -104,16 +105,15 @@ informations : voir https://www.facebook.com/events/189882631576619/?active_tab=
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-xs-6">
+                    <div class="col-xs-offset-2 col-xs-4">
                         <img src="images/JDR/d&d.jpg" class="img-responsive center-block img">
                     </div>
-                    <div class="col-xs-6">
+                    <div class="col-xs-4">
                         <img src="images/JDR/JDRCthulhu.jpg" class="img-responsive center-block img">
                     </div>
                 </div>
             </div>
         </div>
-    </div>
     </div>
 </main>
 <aside>
--- form.php
+++ form.php
@@ -2,140 +2,102 @@
 <html lang="en">
 <head>
     <?php require 'includes/head.html'; ?>
-<!--    <link rel="stylesheet" href="./style/border.css" media="screen,projection" type="text/css"> // si bordure-->
+    <!--    si bordure-->
+    <!--    <link rel="stylesheet" href="./style/border.css" media="screen,projection" type="text/css">-->
     <title>Inscription</title>
 </head>
-<<<<<<< HEAD
 <body>
+<?php require 'includes/header.html'; ?>
 <?php session_start(); ?>
 <form method="post" action="check/checkForm.php">
-    <p>
-        <input type="radio" id="M" name="civ" value="M"> <label for="M">M</label>
-        <input type="radio" id="Mme" name="civ" value="Mme"> <label for="Mme">Mme</label>
-        <input type="radio" id="Mlle" name="civ" value="Mlle"> <label for="Mlle">Mlle</label>
-    </p>
-    <p>
-        <label for="nom">Entrez votre nom :</label>
-        <input type="text" id="nom" name="nom" value="<?php if (isset ($_SESSION['nom'])){ echo  $_SESSION['nom']; } ?>">
-    </p>
-    <p>
-        <label for="prenom">Entrez votre prenom :</label>
-        <input type="text" id="prenom" name="prenom" value="<?php if (isset ($_SESSION['prenom'])){ echo  $_SESSION['prenom']; } ?>">
-    </p>
-    <p>
-        <label for="adresse">Entrez votre Adresse :</label>
-        <input type="text" id="adresse" name="adresse" value="<?php if (isset ($_SESSION['adresse'])){ echo  $_SESSION['adresse']; } ?>">
-    </p>
-    <p>
-        <label for="cp">Entrez votre code postal :</label>
-        <input type="text" id="cp" name="cp" maxlength="5" size="5" value="<?php if (isset ($_SESSION['cp'])){ echo  $_SESSION['cp']; } ?>">
-    </p>
-    <p>
-        <label for="ville">Entrez votre ville :</label>
-        <input type="text" id="ville" name="ville" value="<?php if (isset ($_SESSION['ville'])){ echo  $_SESSION['ville']; } ?>">
-    </p>
-    <p>
-        <label for="dateNaissance">Votre date de naissance :</label>
-        <input type="date" id="dateNaissance" name="dateNaissance" value="<?php if (isset ($_SESSION['dateNaissance'])){ echo  $_SESSION['dateNaissance']; } ?>">
-    </p>
-    <p>
-        <label for="email">Entrez votre adresse mail :</label>
-        <input type="email" id="email" name="email" size="25" value="<?php if (isset ($_SESSION['email'])){ echo  $_SESSION['email']; } ?>">
-    </p>
-    <p>
-        Êtes-vous membre ?
-        <input type="radio" id="Oui" name="membre" value="1"> <label for="Oui">Oui</label>
-        <input type="radio" id="Non" name="membre" value="0"> <label for="Non">Non</label>
-    </p>
-    <p>
-        Jeux : </br>
-        <input name="jeux[]" type="checkbox" id ="jdc" value="jeu de carte"> <label for="jdc">Cartes</label>
-        <input name="jeux[]" type="checkbox" id ="jdp" value="jeu de plateau"> <label for="jdp">Plateau</label>
-        <input name="jeux[]" type="checkbox" id ="jdr" value="jeu de rôle"> <label for="jdr">Jeu de Rôle</label>
-        </select>
-    </p>
-    <button type="submit">S'inscrire</button>
-</form>
-=======
-<body class="container-fluid">
-<header>
-    <?php require 'includes/header.html'; ?>
-</header>
-<main>
-    <form method="post" action="checkForm.php">
-        <div class="container MainC">
-            <div class="row">
-                <div class="col-xs-12">
-                    <div class="row">
-                        <div class="col top left">
-                            <input type="radio" id="M" name="civ" value="M"> <label for="M">M</label>
-                            <input type="radio" id="Mme" name="civ" value="Mme"> <label for="Mme">Mme</label>
-                            <input type="radio" id="Mlle" name="civ" value="Mlle"> <label for="Mlle">Mlle</label>
-                        </div>
-                        <div class="col top left">
-                            <label for="nom">Entrez votre nom :</label>
-                            <input type="text" id="nom" name="nom">
-                        </div>
-                        <div class="col top left">
-                            <label for="prenom">Entrez votre prenom :</label>
-                            <input type="text" id="prenom" name="prenom">
-                        </div>
-                        <div class="col top left">
-                            <label for="adresse">Entrez votre Adresse :</label>
-                            <input type="text" id="adresse" name="adresse">
-                        </div>
-                        <div class="col top left">
-                            <label for="cp">Entrez votre code postal :</label>
-                            <input type="text" id="cp" name="cp" maxlength="5" size="5">
-                        </div>
-                        <div class="col top left">
-                            <label for="ville">Entrez votre ville :</label>
-                            <input type="text" id="ville" name="ville">
-                        </div>
-                        <div class="col top left">
-                            <label for="dateNaissance">Votre date de naissance :</label>
-                            <input type="date" id="dateNaissance" name="dateNaissance">
-                        </div>
-                        <div class="col top left">
-                            <label for="email">Entrez votre adresse mail :</label>
-                            <input type="email" id="email" name="email">
-                        </div>
-                        <div class="col-xs-3 top left">
-                            Êtes-vous membre ?
-                        </div>
-                        <div class="col-xs-3 top left">
-                            <input type="radio" id="Oui" name="membre" value="1"> <label for="Oui">Oui</label>
-                        </div>
-                        <div class="col-xs-3 top left">
-                            <input type="radio" id="Non" name="membre" value="0"> <label for="Non">Non</label>
-                        </div>
-                        <div class="col-xs-12 left">
-                            <h4>Jeux :</h4>
-                        </div>
-                        <div class="col-xs-4 top left">
-                            <input name="jeux[]" type="checkbox" id="jdc" value="jdc"> <label for="jdc">Cartes <img
-                                        src="./images/jeu-de-cartes.jpg"></label>
-                        </div>
-                        <div class="col-xs-4 top left">
-                            <input name="jeux[]" type="checkbox" id="jdp" value="jdp"> <label for="jdp">Plateau <img
-                                        src="./images/summonerWar.jpg"></label>
-                        </div>
-                        <div class="col-xs-4 top left">
-                            <input name="jeux[]" type="checkbox" id="jdr" value="jdr"> <label for="jdr">Jeu de Rôle <img
-                                        src="./images/des-jeu-de-roles.jpg"></label>
-                        </div>
-                        </select>
+    <div class="container MainC">
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="row">
+                    <div class="col-xs-offset-4  col top left">
+                        <input type="radio" id="M" name="civ" value="M"> <label for="M">M</label>
+                        <input type="radio" id="Mme" name="civ" value="Mme"> <label for="Mme">Mme</label>
+                        <input type="radio" id="Mlle" name="civ" value="Mlle"> <label for="Mlle">Mlle</label>
                     </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-offset-5 col-xs-2 top">
-                    <button type="submit">S'inscrire</button>
+                    <div class="col-xs-offset-4  col top left">
+                        <label for="nom">Entrez votre nom :</label>
+                        <input type="text" id="nom" name="nom" value="<?php if (isset ($_SESSION['nom'])) {
+                            echo $_SESSION['nom'];
+                        } ?>">
+                    </div>
+                    <div class="col-xs-offset-4  col top left">
+                        <label for="prenom">Entrez votre prenom :</label>
+                        <input type="text" id="prenom" name="prenom" value="<?php if (isset ($_SESSION['prenom'])) {
+                            echo $_SESSION['prenom'];
+                        } ?>">
+                    </div>
+                    <div class="col-xs-offset-4  col top left">
+                        <label for="adresse">Entrez votre Adresse :</label>
+                        <input type="text" id="adresse" name="adresse" value="<?php if (isset ($_SESSION['adresse'])) {
+                            echo $_SESSION['adresse'];
+                        } ?>">
+                    </div>
+                    <div class="col-xs-offset-4  col top left">
+                        <label for="cp">Entrez votre code postal :</label>
+                        <input type="text" id="cp" name="cp" maxlength="5" size="5"
+                               value="<?php if (isset ($_SESSION['cp'])) {
+                                   echo $_SESSION['cp'];
+                               } ?>">
+                    </div>
+                    <div class="col-xs-offset-4  col top left">
+                        <label for="ville">Entrez votre ville :</label>
+                        <input type="text" id="ville" name="ville" value="<?php if (isset ($_SESSION['ville'])) {
+                            echo $_SESSION['ville'];
+                        } ?>">
+                    </div>
+                    <div class="col-xs-offset-4  col top left">
+                        <label for="dateNaissance">Votre date de naissance :</label>
+                        <input type="date" id="dateNaissance" name="dateNaissance"
+                               value="<?php if (isset ($_SESSION['dateNaissance'])) {
+                                   echo $_SESSION['dateNaissance'];
+                               } ?>">
+                    </div>
+                    <div class="col-xs-offset-4  col top left">
+                        <label for="email">Entrez votre adresse mail :</label>
+                        <input type="email" id="email" name="email" size="25"
+                               value="<?php if (isset ($_SESSION['email'])) {
+                                   echo $_SESSION['email'];
+                               } ?>">
+                    </div>
+                    <div class="col-xs-offset-4  col-xs-2 top left">
+                        <h5>Êtes-vous membre ?</h5>
+                    </div>
+                    <div class="col-xs-1 top left">
+                        <input type="radio" id="Oui" name="membre" value="1"> <label for="Oui">Oui</label>
+                    </div>
+                    <div class="col-xs-1 top left">
+                        <input type="radio" id="Non" name="membre" value="0"> <label for="Non">Non</label>
+                    </div>
+                    <div class="col-xs-12 left center">
+                        <h4>Jeux :</h4>
+                    </div>
+                    <div class="col-xs-offset-2 col-xs-3 top left">
+                        <input name="jeux[]" type="checkbox" id="jdc" value="jeu de carte"> <label
+                                for="jdc">Cartes <img src="./images/miniatures/jeu-de-cartes.jpg"></label>
+                    </div>
+                    <div class="col-xs-3 top left">
+                        <input name="jeux[]" type="checkbox" id="jdp" value="jeu de plateau"> <label
+                                for="jdp">Plateau <img src="./images/miniatures/summonerWar.jpg"</label>
+                    </div>
+                    <div class="col-xs-3 top left">
+                        <input name="jeux[]" type="checkbox" id="jdr" value="jeu de rôle"> <label for="jdr">Jeu de
+                            Rôle <img src="./images/miniatures/des-jeu-de-roles.jpg"</label>
+                    </div>
+                    </select>
                 </div>
             </div>
         </div>
-    </form>
-</main>
->>>>>>> 2bd5c4d902098c25217bfe9aca370b8c4518ef9c
+        <div class="row">
+            <div class="col-xs-12 top center">
+                <button type="submit">S'inscrire</button>
+            </div>
+        </div>
+    </div>
+</form>
 </body>
 </html>
