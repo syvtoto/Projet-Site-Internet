a30ee5f42821ad1d5b652f4e47781e805b139d8c
PC-THOMAS\Thomas <thomas.flamant96@gmail.com>
2018-01-05 19:09:26 +0100

    Voila voila

--- Index.php
+++ Index.php
@@ -1,19 +1,9 @@
 <!DOCTYPE html>
 
-<!-- choses a faire
-
-informations : voir https://www.facebook.com/events/189882631576619/?active_tab=discussion
-- date a inventer (17-18 originelement)
-- 19:00 - 06:00
-- ECE Paris Ecole d'Ingenieur, Immeuble Pollux 37 Quai de grenelle 71520,75015 Paris
--->
-
 <html lang="fr,en">
 <head>
     <?php require 'includes/head.html'; ?>
     <link rel="stylesheet" href="./style/Index.css" media="screen,projection" type="text/css">
-<!--    <link rel="stylesheet" href="./style/border.css" media="screen,projection" type="text/css">-->
-<!--    si bordure-->
     <title>Deuxième Soirée Jeux</title>
 </head>
 <body class="container-fluid">
@@ -24,7 +14,7 @@ informations : voir https://www.facebook.com/events/189882631576619/?active_tab=
     <div class="container">
         <div class="row">
             <div class="col-xs-3 top">
-                <img src="./images/iconeGG.jpg" class="img-responsive center-block img">
+                <img src="./images/iconeGG.png" class="img-responsive center-block img">
                 <div class="center top">
                     <p>20 - 21 Janvier 2018</p>
                     <p>19:00 - 06:00</p>
@@ -32,9 +22,8 @@ informations : voir https://www.facebook.com/events/189882631576619/?active_tab=
                     <p>Immeuble Pollux</p>
                     <p>37 Quai de Grenelle</p>
                     <p>75015 Paris</p>
+                    <p><a href="https://www.google.fr/maps/dir//37+Quai+de+Grenelle,+75015+Paris/@48.8383532,2.239556,13z/data=!4m9!4m8!1m0!1m5!1m1!1s0x47e67004a52552e3:0x47d6b1ac0c239d3c!2m2!1d2.2863606!2d48.8518837!3e3" target="_blank">Accès</a> </p>
                 </div>
-            </div>
-            <div class="col-xs-9 MainC">
                 <div class="row">
                     <div class="col-xs-12 center top">
                         <p>De nombreux jeux seront mis a disposition tels que :</p>
@@ -58,14 +47,6 @@ informations : voir https://www.facebook.com/events/189882631576619/?active_tab=
                             <div class="col-xs-4">
                                 <img src="images/Plateau/les-contrees-de-l-horreur.jpg"
                                      class="img-responsive center-block img">
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-xs-offset-2 col-xs-4">
-                                <img src="images/Plateau/horreur-a-arkham.jpg" class="img-responsive center-block img">
-                            </div>
-                            <div class="col-xs-4">
-                                <img src="images/Plateau/Captain_Sonar.jpg" class="img-responsive center-block img">
                             </div>
                         </div>
                     </div>
@@ -110,6 +91,54 @@ informations : voir https://www.facebook.com/events/189882631576619/?active_tab=
                     </div>
                     <div class="col-xs-4">
                         <img src="images/JDR/JDRCthulhu.jpg" class="img-responsive center-block img">
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-9 MainC">
+                <div class="row center">
+                    <div class="col-xs-12 top">
+                        <h1>Bienvenue Joueur !</h1>
+                    </div>
+                    <div class="col-xs-12 top">
+                        <p>Que tu soit elfe, nain, gnome, ou encore gobelin,</p>
+                        <p>paladin, magicien, barde, ou bien assassin,</p>
+                        <p>nous te convions a notre deuxième soirée jeux.</p>
+                        <p>PS : Les sang-mêlé et super Munchkin seront tolérés</p>
+                        <p>mais pas de bagarre autorisée.</p>
+                    </div>
+                    <div class="col-xs-6 top">
+                        <p>De nombreux jeux de cartes et de plateau</p>
+                        <p>seront disponibles.</p>
+                        <p>Dès jeux de cartes les plus simples comme la tarot</p>
+                        <p>aux jeux de plateau plus complexe tel que</p>
+                        <p>Galaxy Trucker ou bien d'autres.</p>
+                        <p>Le plus important étant que</p>
+                    </div>
+                    <div class="col-xs-6 top">
+                        <p>Si tu est interesser par des jeux de rôles pas de souci !</p>
+                        <p>Nos MJ trouverons de quoi ravire ton âme de rôliste.</p>
+                        <p>Jeter des boules des feu ou encore trancher des</p>
+                        <p>crânes a grand coup de haches, tel sont </p>
+                        <p>les activités des aventuriers.</p>
+                        <p>Le plus important étant que</p>
+                    </div>
+                    <div class="col-xs-12 top">
+                        <p>tu pourra te divertir et profiter de ta soirée.</p>
+                    </div>
+                    <div class="col-xs-12 top">
+                        <p>Débutant ou habitué,</p>
+                        <p>Si tu est intéressé, rejoint nous en passant par la</p>
+                        <p>salle de recrutement, <a href="form.php">ici</a></p>
+                        <p>Tu y remplira des informations qui nous permettrons</p>
+                        <p>de te tenir au courant des prochains évènements.</p>
+                    </div>
+                    <div class="col-xs-12 top">
+                        <p>Nous t'attendrons le 20 janvier au soir</p>
+                        <p>a partir de 21 heures,</p>
+                        <p>37 quai de Grenelle, Immeuble Pollux.</p>
+                    </div>
+                    <div class="col-xs-12 img">
+                        <img src="./images/iYU.png">
                     </div>
                 </div>
             </div>
