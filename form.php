2d0b8956a0d46bd2c7ea4b494f35bd28ce0d4537
PC-THOMAS\Thomas <thomas.flamant96@gmail.com>
2018-01-05 17:18:04 +0100

    design login ok

--- form.php
+++ form.php
@@ -2,8 +2,6 @@
 <html lang="en">
 <head>
     <?php require 'includes/head.html'; ?>
-    <!--    si bordure-->
-<!--            <link rel="stylesheet" href="./style/border.css" media="screen,projection" type="text/css">-->
     <title>Inscription</title>
 </head>
 <body>
@@ -14,93 +12,89 @@
         <div class="row">
             <div class="col-xs-offset-2 col-xs-8 MainC">
                 <div class="row">
-                    <div class="col-xs-12">
-                        <div class="row">
-                            <div class="col-xs-offset-3  col top left">
-                                <input type="radio" id="M" name="civ" value="M"> <label for="M">M</label>
-                                <input type="radio" id="Mme" name="civ" value="Mme"> <label for="Mme">Mme</label>
-                                <input type="radio" id="Mlle" name="civ" value="Mlle"> <label for="Mlle">Mlle</label>
-                            </div>
-                            <div class="col-xs-offset-3  col top left">
-                                <label for="nom">Entrez votre nom :</label>
-                                <input type="text" id="nom" name="nom" value="<?php if (isset ($_SESSION['nom'])) {
-                                    echo $_SESSION['nom'];
-                                } ?>">
-                            </div>
-                            <div class="col-xs-offset-3  col top left">
-                                <label for="prenom">Entrez votre prenom :</label>
-                                <input type="text" id="prenom" name="prenom"
-                                       value="<?php if (isset ($_SESSION['prenom'])) {
-                                           echo $_SESSION['prenom'];
-                                       } ?>">
-                            </div>
-                            <div class="col-xs-offset-3  col top left">
-                                <label for="adresse">Entrez votre Adresse :</label>
-                                <input type="text" id="adresse" name="adresse"
-                                       value="<?php if (isset ($_SESSION['adresse'])) {
-                                           echo $_SESSION['adresse'];
-                                       } ?>">
-                            </div>
-                            <div class="col-xs-offset-3  col top left">
-                                <label for="cp">Entrez votre code postal :</label>
-                                <input type="text" id="cp" name="cp" maxlength="5" size="5"
-                                       value="<?php if (isset ($_SESSION['cp'])) {
-                                           echo $_SESSION['cp'];
-                                       } ?>">
-                            </div>
-                            <div class="col-xs-offset-3  col top left">
-                                <label for="ville">Entrez votre ville :</label>
-                                <input type="text" id="ville" name="ville"
-                                       value="<?php if (isset ($_SESSION['ville'])) {
-                                           echo $_SESSION['ville'];
-                                       } ?>">
-                            </div>
-                            <div class="col-xs-offset-3  col top left">
-                                <label for="dateNaissance">Votre date de naissance :</label>
-                                <input type="date" id="dateNaissance" name="dateNaissance"
-                                       value="<?php if (isset ($_SESSION['dateNaissance'])) {
-                                           echo $_SESSION['dateNaissance'];
-                                       } ?>">
-                            </div>
-                            <div class="col-xs-offset-3  col top left">
-                                <label for="email">Entrez votre adresse mail :</label>
-                                <input type="email" id="email" name="email" size="25"
-                                       value="<?php if (isset ($_SESSION['email'])) {
-                                           echo $_SESSION['email'];
-                                       } ?>">
-                            </div>
-                            <div class="col-xs-offset-3  col-xs-2 top left">
-                                <h5>Êtes-vous membre ?</h5>
-                            </div>
-                            <div class="col-xs-1 top left">
-                                <input type="radio" id="Oui" name="membre" value="1"> <label for="Oui">Oui</label>
-                            </div>
-                            <div class="col-xs-1 top left">
-                                <input type="radio" id="Non" name="membre" value="0"> <label for="Non">Non</label>
-                            </div>
-                            <div class="col-xs-offset-3 col-xs-6 left center">
-                                <h4>Jeux :</h4>
-                            </div>
-                            <div class="col-xs-offset-2 col-xs-3 top left">
-                                <input name="jeux[]" type="checkbox" id="jdc" value="jeu de carte"> <label
-                                        for="jdc">Cartes <img src="./images/miniatures/jeu-de-cartes.jpg"></label>
-                            </div>
-                            <div class="col-xs-3 top left">
-                                <input name="jeux[]" type="checkbox" id="jdp" value="jeu de plateau"> <label
-                                        for="jdp">Plateau <img src="./images/miniatures/summonerWar.jpg"</label>
-                            </div>
-                            <div class="col-xs-3 top left">
-                                <input name="jeux[]" type="checkbox" id="jdr" value="jeu de rôle"> <label for="jdr">Jeu
-                                    de
-                                    Rôle <img src="./images/miniatures/des-jeu-de-roles.jpg"</label>
-                            </div>
-                            <select id="jeux[]">
-                                <option value="jeu de carte">Cartes</option>
-                                <option value="jeu de plateau" selected>Plateau</option>
-                                <option value="jeu de rôle">Jeu de rôle</option>
-                            </select>
-                        </div>
+                    <div class="col-xs-offset-3  col top left">
+                        <input type="radio" id="M" name="civ" value="M"> <label for="M">M</label>
+                        <input type="radio" id="Mme" name="civ" value="Mme"> <label for="Mme">Mme</label>
+                        <input type="radio" id="Mlle" name="civ" value="Mlle"> <label for="Mlle">Mlle</label>
                     </div>
+                    <div class="col-xs-offset-3  col top left">
+                        <label for="nom">Entrez votre nom :</label>
+                        <input type="text" id="nom" name="nom" value="<?php if (isset ($_SESSION['nom'])) {
+                            echo $_SESSION['nom'];
+                        } ?>">
+                    </div>
+                    <div class="col-xs-offset-3  col top left">
+                        <label for="prenom">Entrez votre prenom :</label>
+                        <input type="text" id="prenom" name="prenom"
+                               value="<?php if (isset ($_SESSION['prenom'])) {
+                                   echo $_SESSION['prenom'];
+                               } ?>">
+                    </div>
+                    <div class="col-xs-offset-3  col top left">
+                        <label for="adresse">Entrez votre Adresse :</label>
+                        <input type="text" id="adresse" name="adresse"
+                               value="<?php if (isset ($_SESSION['adresse'])) {
+                                   echo $_SESSION['adresse'];
+                               } ?>">
+                    </div>
+                    <div class="col-xs-offset-3  col top left">
+                        <label for="cp">Entrez votre code postal :</label>
+                        <input type="text" id="cp" name="cp" maxlength="5" size="5"
+                               value="<?php if (isset ($_SESSION['cp'])) {
+                                   echo $_SESSION['cp'];
+                               } ?>">
+                    </div>
+                    <div class="col-xs-offset-3  col top left">
+                        <label for="ville">Entrez votre ville :</label>
+                        <input type="text" id="ville" name="ville"
+                               value="<?php if (isset ($_SESSION['ville'])) {
+                                   echo $_SESSION['ville'];
+                               } ?>">
+                    </div>
+                    <div class="col-xs-offset-3  col top left">
+                        <label for="dateNaissance">Votre date de naissance :</label>
+                        <input type="date" id="dateNaissance" name="dateNaissance"
+                               value="<?php if (isset ($_SESSION['dateNaissance'])) {
+                                   echo $_SESSION['dateNaissance'];
+                               } ?>">
+                    </div>
+                    <div class="col-xs-offset-3  col top left">
+                        <label for="email">Entrez votre adresse mail :</label>
+                        <input type="email" id="email" name="email" size="25"
+                               value="<?php if (isset ($_SESSION['email'])) {
+                                   echo $_SESSION['email'];
+                               } ?>">
+                    </div>
+                    <div class="col-xs-offset-3  col-xs-2 top left">
+                        <h5>Êtes-vous membre ?</h5>
+                    </div>
+                    <div class="col-xs-1 top left">
+                        <input type="radio" id="Oui" name="membre" value="1"> <label for="Oui">Oui</label>
+                    </div>
+                    <div class="col-xs-1 top left">
+                        <input type="radio" id="Non" name="membre" value="0"> <label for="Non">Non</label>
+                    </div>
+                    <div class="col-xs-offset-3 col-xs-6 left center">
+                        <h4>Jeux :</h4>
+                    </div>
+                    <div class="col-xs-offset-2 col-xs-3 top left">
+                        <input name="jeux[]" type="checkbox" id="jdc" value="jeu de carte"> <label
+                                for="jdc">Cartes <img src="./images/miniatures/jeu-de-cartes.jpg"></label>
+                    </div>
+                    <div class="col-xs-3 top left">
+                        <input name="jeux[]" type="checkbox" id="jdp" value="jeu de plateau"> <label
+                                for="jdp">Plateau <img src="./images/miniatures/summonerWar.jpg"</label>
+                    </div>
+                    <div class="col-xs-3 top left">
+                        <input name="jeux[]" type="checkbox" id="jdr" value="jeu de rôle"> <label for="jdr">Jeu
+                            de
+                            Rôle <img src="./images/miniatures/des-jeu-de-roles.jpg"</label>
+                    </div>
+                    <select id="jeux[]">
+                        <option value="jeu de carte">Cartes</option>
+                        <option value="jeu de plateau" selected>Plateau</option>
+                        <option value="jeu de rôle">Jeu de rôle</option>
+                    </select>
                 </div>
                 <div class="row">
                     <div class="col-xs-12 top center">
--- login.php
+++ login.php
@@ -2,45 +2,67 @@
 <html lang="en">
 <head>
     <?php require 'includes/head.html'; ?>
-    <title>Connexion</title>
+    <title>Acces MJ</title>
 </head>
 <body>
 <?php
+require 'includes/header.html';
 session_start();
 if (isset ($_SESSION['admin'])):
     session_destroy();
     header('location:index.php');
-    /*header('location :Index.php');
-    echo "Vous êtes déjà connecté";*/
+/*header('location :Index.php');
+echo "Vous êtes déjà connecté";*/
 else :
-    if (isset($_GET['error'])){
-        switch ($_GET['error']) {
-            case 'cahmpVide':
-                $error = "Veuillez renseigner les champs";
-                break;
-            case 'falsePw' :
-                $error ="Mauvais Mot de Passe";
-                break;
-            case 'falseLog' :
-                $error ="Mauvaise Adresse Mail";
-                break;
-            default :
-                $error = "Mauvais identifiants";
-                break;
-        }
-        echo "Erreur : ".$error;
-
+if (isset($_GET['error'])) {
+    switch ($_GET['error']) {
+        case 'cahmpVide':
+            $error = "Veuillez renseigner les champs";
+            break;
+        case 'falsePw' :
+            $error = "Mauvais Mot de Passe";
+            break;
+        case 'falseLog' :
+            $error = "Mauvaise Adresse Mail";
+            break;
+        default :
+            $error = "Mauvais identifiants";
+            break;
     }
-    ?>
-    <h1>Connexion</h1>
-    <form method="post" action="check/check.php" enctype="multipart/form-data">
-        <label for="login">Renseigner votre login :</label><br>
-        <input id="login" name="login" type="text" placeholder="Login" required="true"
-               value="<?php if (isset ($_GET['login'])){ echo  $_GET['login']; } ?>"></br>
-        <label for="password">Entrez votre mot de passe :</label><br>
-        <input id="password" name="pw" type="password" placeholder="Password"><br>
-        <input type="submit" value="Se Connecter">
-    </form>
-<?php endif ;?>
+    echo "Erreur : " . $error;
+
+}
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-xs-offset-4 col-xs-4 MainC center">
+            <div class="row">
+                <div class="col img top">
+                    <h1>Connexion</h1>
+                    <form method="post" action="check/check.php" enctype="multipart/form-data">
+                </div>
+                <div class="col img top">
+                    <label for="login">Nom :</label>
+                </div>
+                <div class="col img">
+                    <input id="login" name="login" type="text" placeholder="Nom" required="true"
+                           value="<?php if (isset ($_GET['login'])) {
+                               echo $_GET['login'];
+                           } ?>">
+                </div>
+                <div class="col img top">
+                    <label for="password">Token :</label><br>
+                </div>
+                <div class="col img">
+                    <input id="password" name="pw" type="password" placeholder="Token"><br>
+                </div>
+                <div class="col top img">
+                    <input type="submit" value="Entrer">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php endif; ?>
 </body>
 </html>
